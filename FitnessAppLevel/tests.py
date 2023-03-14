88acd4e8e1b8de78fa91306fc33dd065aedee55b
citysushi89 <owenpickard89@gmail.com>
2023-03-14 19:33:40 -0400

    Initial Commit

---
+++ FitnessAppLevel/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.urls import resolve
+from .views import home
+
+# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
+
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'FitnessAppLevel/home.html')
